37c5eec9afb082ce66e7f98e700e2667e6df23c8
Xiangyu Liu <neulxy@gmail.com>
2022-11-03 11:03:41 +1300

    split app component into cockpit and server-element components

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -3,13 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cockpit',
   templateUrl: './cockpit.component.html',
-  styleUrls: ['./cockpit.component.css']
+  styleUrls: ['./cockpit.component.css'],
 })
 export class CockpitComponent implements OnInit {
+  constructor() {}
+  newServerName = '';
+  newServerContent = '';
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onAddServer() {
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent,
+    });
   }
 
+  onAddBlueprint() {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent,
+    });
+  }
 }
